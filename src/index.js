971d2b0839f191f6a36d977b9726a72bc0b12629
Milan Pjevic <milanpjevic@icloud.com>
2024-11-13 17:43:00 +0100

    Nesting Components

--- src/index.js
+++ src/index.js
@@ -51,7 +51,21 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
@@ -68,6 +82,12 @@ function Pizza() {
     </div>
   );
 }
+
+const Footer = () => {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
